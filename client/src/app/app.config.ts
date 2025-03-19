11441851270534d324a5f181fdf6d6a532d1cce3
tobezero01 <ducnhuad@gmail.com>
2025-03-19 23:57:28 +0700

    add interceptor to store JWTToken for login session

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -6,11 +6,12 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from './_interceptor/error.interceptor';
+import { jwtInterceptor } from './_interceptor/jwt.interceptor';
 export const appConfig: ApplicationConfig = {
   providers:
   [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])),
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right'
