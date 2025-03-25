e4a1bceb536ffddd32bd6566caa1e647b03050ca
tobezero01 <ducnhuad@gmail.com>
2025-03-26 00:26:23 +0700

    update load animation

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,20 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router'
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from './_interceptor/error.interceptor';
 import { jwtInterceptor } from './_interceptor/jwt.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { loadingInterceptor } from './_interceptor/loading.interceptor';
+
 export const appConfig: ApplicationConfig = {
   providers:
   [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor, loadingInterceptor])),
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right'
-    })
+    }),
+    importProvidersFrom(NgxSpinnerModule)
   ]
 };
